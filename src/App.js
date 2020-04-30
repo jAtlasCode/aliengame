18c70a0f209e99c64d105eb9e29fd5f80c1d8a7d
JMosaic <jra297@nau.edu>
2020-04-30 16:39:18 -0700

    props broken /:

--- src/App.js
+++ src/App.js
@@ -1,27 +1,95 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types'
-import Canvas from './components/Canvas';
-import { getCanvasPosition } from './utils/formulas';
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import Canvas from "./components/Canvas";
+import { getCanvasPosition } from "./utils/formulas";
 
-
+window.onresize = () => {
+  const cnv = document.getElementById("aliens-go-home-canvas");
+  cnv.style.width = `${window.innerWidth}px`;
+  cnv.style.height = `${window.innerHeight}px`;
+};
 
 function App(props) {
-
+  const canvasMousePos = useRef();
   useEffect(() => {
-    setInterval(() => {
-      self.props.moveObjects(self.canvasMousePosition);
-    }, 10)
-  }, [])
+    console.log(props.gameState)
+    window.onresize();
+    const id = setInterval(() => props.moveObjects(canvasMousePos.current));
+    return () => clearInterval(id);
+  }, [props]);
+
+  const trackMouse = (event) => {
+    canvasMousePos.current = getCanvasPosition(event);
+  };
 
   return (
     <div className="App">
-        <Canvas />
+      <Canvas
+        angle={props.angle}
+        trackMouse={trackMouse}
+        gameState={props.gameState}
+        startGame={props.startGame}
+      />
     </div>
   );
 }
 
-// App.propTypes = {
-//   message: PropTypes.string.isRequired,
-// };
+App.propTypes = {s
+  angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+      position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired,
+      id: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
+  moveObjects: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
+};
 
+// App.propTypes = {
+//   angle: PropTypes.number.isRequired,
+//   gameState: PropTypes.shape({
+//     started: PropTypes.bool.isRequired,
+//     kills: PropTypes.number.isRequired,
+//     lives: PropTypes.number.isRequired,
+//     flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+//       position: PropTypes.shape({
+//         x: PropTypes.number.isRequired,
+//         y: PropTypes.number.isRequired
+//       }).isRequired,
+//       id: PropTypes.number.isRequired,
+//     })).isRequired,
+//     // ... other propTypes definitions ...
+//   }).isRequired,
+//   // ... other propTypes definitions ...
+// };
 export default App;
+
+// class App extends React.Component {
+//   componentDidMount() {
+//     const self = this;
+//     console.log(self)
+//     setInterval(() => {
+//         self.props.moveObjects(self.canvasMousePosition);
+//     }, 10);
+//   }
+
+//   trackMouse(event) {
+//     this.canvasMousePosition = getCanvasPosition(event);
+//   }
+
+//   render() {
+//     return (
+//       <Canvas
+//         angle={this.props.angle}
+//         trackMouse={event => (this.trackMouse(event))}
+//       />
+//     );
+//   }
+// }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,15 @@
 // wide for fullscreen feeling
 export const skyAndGroundWidth = 5000;
+export const gameWidth = 800;
+export const createInterval = 1000;
+export const gameHeight = 1200;
+export const maxFlyingObjects = 4;
+export const flyingObjectsStarterYAxis = -1100;
+// export const flyingObjectsStarterYAxis = -1000;
+
+export const flyingObjectsStarterPositions = [
+  -300,
+  -150,
+  150,
+  300,
+];
